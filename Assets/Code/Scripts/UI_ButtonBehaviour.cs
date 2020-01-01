684a1da959d79cd4f56572c6a5c30025baf09ae6
SKY <akash.rm.roy@gmail.com>
2020-01-01 15:33:08 +0530

    Add files via upload

---
+++ Assets/Code/Scripts/UI_ButtonBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ButtonBehaviour : MonoBehaviour
+{
+    //public GameObject button;
+    public int inactiveDuration = 1;
+
+    private Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();//Component gets the button from the Button object itself during the first frame
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!(button.interactable))
+        {
+            //wait for 1seconds implementation
+            StartCoroutine(ChangeState(1));
+        }
+    }
+
+    IEnumerator ChangeState(int inactiveDuration)
+    {
+        //wait for 1 second and then set it button.interactable = true
+        yield return new WaitForSeconds(inactiveDuration);
+        button.interactable = true;
+    }
+}
